4e3cac9688a8be7a09b119d3eec5b9f02b49cdad
Manuel Tomas Teves <manuel.tomas.teves@gmail.com>
2026-03-25 18:08:24 -0300

    A2 fixed

--- TPN1/A2_WayTooLongWords/src/app.cpp
+++ TPN1/A2_WayTooLongWords/src/app.cpp
@@ -2,13 +2,18 @@
 #define FIN std::ios::sync_with_stdio(0);std::cin.tie(0);std::cout.tie(0) // Upgrade std::cin & std::cout velocity
 int main() {
     FIN;
-    std::string s;
-    std::cin >> s;
-    int l = s.length();
-    if (l > 10) {
-        std::cout << s[0] << s.length() - 2 << s[l - 1] << "\n";
-    } else {
-        std::cout << s << "\n";
+    int quantity;
+    std::cin >> quantity;
+    std::string s[quantity];
+    for(int i = 0; i < quantity; i++)
+        std::cin >> s[i];
+    for(int i = 0; i < quantity; i++) {
+        int l = s[i].length();
+        if (l > 10) {
+            std::cout << s[i][0] << l - 2 << s[i][l - 1] << "\n";
+        } else {
+            std::cout << s[i] << "\n";
+        }
     }
     return 0;
 }
